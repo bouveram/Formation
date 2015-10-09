33afdc7728c2a71680c2c3641097e31b2f77251c
bouveram <maximebouverat@gmail.com>
2015-10-09 14:54:26 +0200

    Interface creation

---
+++ Java/src/com/m2i/formation/media/IItem.java
@@ -0,0 +1,17 @@
+package com.m2i.formation.media;
+
+public interface IItem {
+
+	double getPrice();
+
+	int getId();
+
+	String getName();
+
+	void setPrice(double price);
+
+	void setId(int id);
+
+	void setName(String name);
+
+}
--- Java/src/com/m2i/formation/media/Item.java
+++ Java/src/com/m2i/formation/media/Item.java
@@ -1,27 +1,51 @@
 package com.m2i.formation.media;
 
 //Une class Abstract ne peut heriter que d'une classe abstract
-public abstract class Item {
+public abstract class Item implements IItem {
 
 	private double price;
 	private int id;
 	private String name;
 	
+	/* (non-Javadoc)
+	 * @see com.m2i.formation.media.IItem#getPrice()
+	 */
+	@Override
 	public double getPrice() {
 		return price;
 	}
+	/* (non-Javadoc)
+	 * @see com.m2i.formation.media.IItem#getId()
+	 */
+	@Override
 	public int getId() {
 		return id;
 	}
+	/* (non-Javadoc)
+	 * @see com.m2i.formation.media.IItem#getName()
+	 */
+	@Override
 	public String getName() {
 		return name;
 	}
+	/* (non-Javadoc)
+	 * @see com.m2i.formation.media.IItem#setPrice(double)
+	 */
+	@Override
 	public void setPrice(double price) {
 		this.price = price;
 	}
+	/* (non-Javadoc)
+	 * @see com.m2i.formation.media.IItem#setId(int)
+	 */
+	@Override
 	public void setId(int id) {
 		this.id = id;
 	}
+	/* (non-Javadoc)
+	 * @see com.m2i.formation.media.IItem#setName(java.lang.String)
+	 */
+	@Override
 	public void setName(String name) {
 		this.name = name;
 	}
