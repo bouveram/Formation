e2a943f847724633fcaa4df9c584d80d77891987
bouveram <maximebouverat@gmail.com>
2015-10-30 15:16:27 +0100

    Get informations from DB

--- Java/src/com/m2i/formation/media/repositories/IRepository.java
+++ Java/src/com/m2i/formation/media/repositories/IRepository.java
@@ -1,6 +1,7 @@
 package com.m2i.formation.media.repositories;
 
 import java.io.IOException;
+import java.sql.SQLException;
 import java.util.List;
 
 import com.m2i.formation.media.entities.*;
@@ -11,11 +12,11 @@ public interface IRepository<T extends IEntity> { // IRepository<T> where T:Medi
 	
 	void setUri(String uri);
 	
-	List<T> getAll() throws IOException;
+	List<T> getAll() throws IOException, SQLException;
 	
-	T getById(int id) throws IOException ;
+	T getById(int id) throws IOException, SQLException ;
 	
-	List<T> getByPrice(double price) throws IOException ;
+	List<T> getByPrice(double price) throws IOException, SQLException ;
 	
 	List<T> getByTitle(String word) throws IOException ;
 }
